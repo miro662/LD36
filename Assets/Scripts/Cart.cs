b9fdab4c8ae5c6a6d73f7b9471aa64c2c97f4886
Mirosław Błażej <miro662@gmail.com>
2016-08-27 09:20:11 +0200

    Added cart jumping

--- Assets/Scripts/Cart.cs
+++ Assets/Scripts/Cart.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class Cart : MonoBehaviour
 {
     #region PARAMETERS
-
     [Header("Track Data")]
     public Track[] tracks;
     public int firstTrack;
@@ -12,27 +11,66 @@ public class Cart : MonoBehaviour
 
     [Header("Cart Parameters")]
     public float horizontalVelocity;
-    public Vector3 cartOffset;
 
+    [Header("Jumping")]
+    public float jumpHeight;
+    public float jumpTime;
     #endregion
 
 
     #region MOVING_METHODS
-    
     void MoveAlong(Track track, float distance)
     {
         Vector3 newPosition = transform.position;
-        newPosition.y = track.transform.position.y; // set position to current track position
+        if (!isJumping)
+        {
+            newPosition.y = track.transform.position.y; // set position to current track position
+        }
+        else
+        {
+            newPosition.y += Jumping() * Time.fixedDeltaTime;
+            if (newPosition.y <= track.transform.position.y)
+            {
+                newPosition.y = track.transform.position.y;
+                isJumping = false;
+            }
+        }
         newPosition.x += distance;
-        newPosition += cartOffset;
         transform.position = newPosition;
     }
-
     #endregion
 
+    #region JUMPING
+    public bool isJumping = false;
+    public void Jump()
+    {
+        if (!isJumping)
+        {
+            isJumping = true;
+            jumpVelocity = JumpForce;
+        }
+    }
+
+    float JumpForce
+    {
+        get { return Gravity * jumpTime; }
+    }
+
+    float Gravity
+    {
+        get { return (2 * jumpHeight) / (jumpTime * jumpTime); }
+    }
+
+    float jumpVelocity;
+
+    float Jumping()
+    {
+        jumpVelocity -= Gravity * Time.fixedDeltaTime;
+        return jumpVelocity;
+    }
+    #endregion
 
     #region MONOBEHAVIOUR_METHODS
-
     void Awake()
     {
         currentTrack = firstTrack;
@@ -44,5 +82,12 @@ public class Cart : MonoBehaviour
         MoveAlong(tracks[currentTrack], horizontalVelocity * Time.fixedDeltaTime);
     }
 
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
     #endregion
 }
