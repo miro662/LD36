70354be5ae0106bc609340185da688f330501858
Mirosław Błażej <miro662@gmail.com>
2016-08-28 18:09:13 +0200

    Added camera

---
+++ Assets/Scripts/CameraFollowX.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowX : MonoBehaviour {
+    public Transform follow;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newPos = transform.position;
+        newPos.x = follow.position.x;
+        transform.position = newPos;
+	}
+}
