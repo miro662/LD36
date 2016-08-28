8f498cc168dc653ba4df717e66f2f3cb7df038fe
Mirosław Błażej <miro662@gmail.com>
2016-08-28 12:49:10 +0200

    Added traps

--- Assets/Scripts/GameManagers/GameManager.cs
+++ Assets/Scripts/GameManagers/GameManager.cs
@@ -1,6 +1,27 @@
 ﻿using Zenject;
+using UnityEngine;
 
-public class GameManager
+public class GameManager : ITickable
 {
+    [Inject]
+    PauseManager pause;
+
     public int points = 0;
+
+    float seconds = 0;
+
+    void ITickable.Tick()
+    {
+        seconds += Time.deltaTime;
+        if (seconds > 1)
+        {
+            seconds -= 1f;
+            points += 1;
+        }
+    }
+
+    public void Death()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
 }
---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trap : MonoBehaviour
+{
+    Collider2D _collider2d;
+    void Awake()
+    {
+        _collider2d = GetComponent<Collider2D>();
+    }
+
+    [Zenject.Inject]
+    GameManager _manager;
+
+    public LayerMask player;
+
+    void Update()
+    {
+        //Try to "catch" player
+        RaycastHit2D hit = Physics2D.BoxCast(_collider2d.bounds.center, _collider2d.bounds.size, 0, Vector2.zero, 0, player);
+        if (hit.collider != null)
+        {
+            _manager.Death();
+        }
+    }
+}
